3e8df37e0de56bcedd109f0d7578cf1fd6967087
Mate Marschalko <webondevices@gmail.com>
2019-09-01 10:33:42 +0100

    feat: add babel, es6, react, webpack dev server

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import css from "./style.css";
+
+const App = () => {
+  return <div>Hello React,Webpack 4 & Babel 7!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
